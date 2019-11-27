bec10d89139bf51d6e1d9b3500e9912d952fc050
kuangshengzuoye <1184668109@qq.com>
2019-11-28 00:50:02 +0800

    first commit

---
+++ src/main/java/com/mmall/mall_user_manager/MallUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mmall.mall_user_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUserManagerApplication.class, args);
+    }
+
+}
